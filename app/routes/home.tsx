3b9e00d2f1897a932dd57b50e6f71d42c0d63545
Franz Vrolijk <franzvrolijk@gmail.com>
2025-02-20 15:51:45 +0100

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,25 @@
-
+import { useContext } from "react";
+import { ThemeContext } from "~/themeContext";
 
 export default function Home() {
-  return <Welcome />;
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return (
+    <div className="h-full w-full flex flex-col justify-center items-center bg-background text-foreground">
+      <p className="text-5xl">current theme: {theme}</p>
+      <div className="m-4 flex gap-2">
+        <ThemeChangeButton themeName="light" setTheme={setTheme} />
+        <ThemeChangeButton themeName="dark" setTheme={setTheme} />
+        <ThemeChangeButton themeName="system" setTheme={setTheme} />
+      </div>
+    </div>
+  );
 }
+
+const ThemeChangeButton = ({ themeName, setTheme }: { themeName: string; setTheme: (theme: string) => void }) => {
+  return (
+    <button className="p-2 rounded bg-foreground text-background" onClick={() => setTheme(themeName)}>
+      {themeName}
+    </button>
+  );
+};
